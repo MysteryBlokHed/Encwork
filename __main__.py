39b49975a911e271edba5f72b8714b571850171e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2020-01-25 19:17:27 -0500

    Reverted __main__.py due to useless and incomplete features.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,7 @@
-# Created by MysteryBlokHed on 03/01/2020.
-from encwork.p2p import P2P
-from encwork.client import Client
+# Created by MysteryBlokHed on 14/12/2019.
+from ttchat.p2p import P2P
 from threading import Thread
 
-# P2P
 class PrintMessages(Thread):
     def __init__(self, chat):
         Thread.__init__(self)
@@ -12,29 +10,13 @@ class PrintMessages(Thread):
     def run(self):
         for msg in self._chat.stream_messages():
             print(msg)
-# Server
 
-# Ask which chat mode to use
+target = input("Enter the target machine IP: ")
+chat = P2P()
+chat.start(target)
+
+PrintMessages(chat).start()
+
 while True:
-    mode = input("Which chat mode is being used? (P2P, Server): ")
-    if mode.upper() in ("P2P", "SERVER"):
-        break
-    else:
-        print("Invalid choice.")
-
-# P2P being used
-if mode.upper() == "P2P":
-    target = input("Enter the target machine IP: ")
-    chat = P2P()
-    chat.start(target)
-
-    PrintMessages(chat).start()
-
-    while True:
-        msg = input("")
-        chat.send_msg(msg)
-# Server-based being used
-elif mode.upper() == "SERVER":
-    pass
-else:
-    print("Invalid chat mode.")
+    msg = input("")
+    chat.send_msg(msg)
